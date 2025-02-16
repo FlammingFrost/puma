e99d5d06e5d08b736677c13f16c2a023f778a0d3
FlammingFrost <12012816@mail.sustech.edu.cn>
2025-02-16 12:47:53 -0800

    Add accuracy calculation for retrieval and update test script LangChain pipeline to generate queries from comments not implemented.

--- tests/test_rag.py
+++ tests/test_rag.py
@@ -41,10 +41,44 @@ def get_retrieval_unstructure(data):
         row['retrieved_file_paths'] = retrieved_file_paths
     
     return data
+
+def cal_accuracy(data):
+    """
+    Calculate the accuracy of the retrieval.
     
+    Args:
+        data (list): List of dictionaries with the selected keys.
+            keys:
+                docstring: The docstring of the code.
+                query: The query string. Transform from docstring.
+                code: The code snippet.
+                func_name: The function name.
+                file_path: The file path. i.e. repo/path/to/file.py
+                retrieved_file_paths: The retrieved file paths.
+                
+    Returns:
+        float: The accuracy of the retrieval.
+    """
+    total = len(data)
+    correct = 0
+    for row in data:
+        if row['file_path'] in row['retrieved_file_paths']:
+            correct += 1
     
-if os.path.exists(file_with_query):
-    data_python = load_data_python(file_with_query, num_rows=100)
-else:
-    data_python_raw = load_data_python(file_path_python, num_rows=100)
-    data_python = generated_query(data_python_raw, num_rows=100, seed=224)
+    return correct / total
+    
+def main():
+    if os.path.exists(file_with_query):
+        data_python = load_data_python(file_with_query, num_rows=100)
+    else:
+        data_python_raw = load_data_python(file_path_python, num_rows=100)
+        data_python = generate_query(data_python_raw, num_rows=100, seed=224)
+        
+    data = get_retrieval_unstructure(data_python)
+    accuracy = cal_accuracy(data)
+    print(f"Accuracy: {accuracy}")
+    with open("tests/data/python/python_test_accuracy", "w") as f:
+        f.write(f"Accuracy: {accuracy}")
+
+if __name__ == "__main__":
+    main()
