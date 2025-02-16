bc0fcd316e6ed1796d8f37dde49b63297fc59038
FlammingFrost <12012816@mail.sustech.edu.cn>
2025-02-15 19:18:27 -0800

    Refactor configuration loading and logging; remove deprecated files and update imports

---
+++ configs/configurator.py
@@ -0,0 +1,40 @@
+"""Reads configuration from settings.yaml file"""
+import yaml
+import os
+from tools.logger import logger
+
+CONFIG_PATH = os.path.join(os.path.dirname(__file__), "settings.yaml")
+API_KEY_PATH = os.path.join(os.path.dirname(__file__), "api_key.yaml")
+
+class ConfigLoader:
+    def __init__(self):
+        self.config = self.load_config()
+
+    def load_config(self):
+        """Loads configuration from settings.yaml"""
+        with open(CONFIG_PATH, "r") as file:
+            config = yaml.safe_load(file)
+        with open(API_KEY_PATH, "r") as file:
+            api_keys = yaml.safe_load(file)
+            config["openai"]["api_key"] = api_keys["openai"]['api_key']
+            config["claude"]["api_key"] = api_keys["claude"]['api_key']
+        
+        # Override API key with environment variable if set
+        OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+        CLAUDE_API_KEY = os.getenv("CLAUDE_API_KEY")
+        if OPENAI_API_KEY:
+            config["openai"]["api_key"] = OPENAI_API_KEY
+        if CLAUDE_API_KEY:
+            config["claude"]["api_key"] = CLAUDE_API_KEY
+        
+        # DEBUG: list all available models from OpenAI
+        import openai
+        openai.api_key = config["openai"]["api_key"]
+        openai_models = openai.models.list()
+        openai_models = [model.id for model in openai_models.data]
+        logger.debug(f"Available OpenAI models:\n {openai_models}")
+        
+        return config
+
+config_loader = ConfigLoader()
+config = config_loader.config
