4e570d808e96093ea264865a5d711d2972eeb371
FlammingFrost <12012816@mail.sustech.edu.cn>
2025-03-06 16:53:50 -0800

    Add training script for mapping block from query than code emb

---
+++ train/train_mapping_block_on_query.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <mapping_block> <residual>"
+    exit 1
+fi
+
+MAPPING_BLOCK=$1
+RESIDUAL=$2
+
+python train/train.py \
+    --mapping_block $MAPPING_BLOCK \
+    --train_data data/python_dataset/train \
+    --eval_data data/python_dataset/valid \
+    --tokenizer_name jinaai/jina-embeddings-v2-base-code \
+    --base_model_name jinaai/jina-embeddings-v2-base-code \
+    --max_len 512 \
+    --epochs 200 \
+    --batch_size 1024 \
+    --learning_rate 2e-5 \
+    --device cuda \
+    --train_query_emb_path models/embeddings/small_train_embeddings_query.pt \
+    --train_code_emb_path models/embeddings/train_embeddings_query.pt \
+    --eval_query_emb_path models/embeddings/small_eval_embeddings_query.pt \
+    --eval_code_emb_path models/embeddings/eval_embeddings_code.pt \
+    --residual $RESIDUAL
